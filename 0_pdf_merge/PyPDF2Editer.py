7f7a372cae4636754a6f273c3045d5d57792f474
s-hirata0831 <e9248@g.maizuru-ct.ac.jp>
2025-10-15 11:02:41 +0900

    :sparkles: Webセキュリティ実験レポートの修正を実施

--- 0_pdf_merge/PyPDF2Editer.py
+++ 0_pdf_merge/PyPDF2Editer.py
@@ -1,7 +1,7 @@
 import PyPDF2
 
 file1 = "/Users/hiratasoma/Documents/Ex_IVB/1_web_security/repo/web_security_front.pdf"
-file2 = "/Users/hiratasoma/Documents/Ex_IVB/1_web_security/repo/web_security.pdf"
+file2 = "/Users/hiratasoma/Documents/Ex_IVB/1_web_security/repo/out/web_security.pdf"
 
 merger = PyPDF2.PdfMerger()
 merger.append(file1)
